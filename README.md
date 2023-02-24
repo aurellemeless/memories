db6395c5b1052b3842202b877484b82460819aa7
Aurelle Meless <aurelle.meless@gmail.com>
2023-02-24 23:41:50 +0100

    add matches feature & effects

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
-# Memories game
-Click on a card and return the card.
+# Memories game 
+- Click on a card and return the card.
+- Remove if cards matches : clic n & clic n-1
+- Count the matches
+- Store the Clic history
+# Some Challenges
+## Create level/stages : 
+- Increase the numbers of cards
+- Reduce the time of animation
+## Improve the UX
+- Add sound and visual effects on matches
+- Add Timer
+
+## Add settings parameters for columns and number of cards 
 # Tools
  React
  Redux
--- src/components/Card/CardTable/index.js
+++ src/components/Card/CardTable/index.js
@@ -1,18 +1,20 @@
 import { useSelector } from "react-redux";
 import { Card } from "..";
-import { selectPosition, selectLatestPositions, selectMatches } from "../../../store/reducers";
+import { selectLatestPositions, selectMatches, selectMatchesStack } from "../../../store/reducers";
 
 export function CardTable({data}){
-    const activePosition = useSelector(selectPosition);
     const latestClickedCards = useSelector(selectLatestPositions) || [];
     const countMatches = useSelector(selectMatches);
+    const macthesStack = useSelector(selectMatchesStack);
     return (
     <div>
-        <div style={{padding:10}}> Memories Game position selectionné { activePosition }  |  Score :  {countMatches} paires trouvée(s) </div>
+        <div style={{padding:10}}> Memories Game |  Score :  {countMatches} paires trouvée(s) </div>
         <div className='card-wrapper'>
             {data.map((card)=><Card key={card.position} 
             model={card} 
-            active={latestClickedCards.includes(card.position)}>
+            active={latestClickedCards.includes(card.position)}
+            matched={macthesStack.includes(card.position)}
+            >
 
             </Card>)}
         </div>
